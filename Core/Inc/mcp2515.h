8ab2e50e9b0a82e77c1d9a90f7b3b82195061bf1
adammarut <adam.marut@icloud.com>
2025-10-15 12:13:04 +0200

    Update mcp2515.h for CAN changes

--- Core/Inc/mcp2515.h
+++ Core/Inc/mcp2515.h
@@ -21,6 +21,7 @@ extern "C" {
 #endif
 
 #include "stm32f4xx_hal.h"
+#include "can_process.h"
 
 /* MCP2515 SPI Instructions */
 #define MCP2515_RESET           0xC0
@@ -120,14 +121,7 @@ typedef enum {
     CAN_1000KBPS
 } CAN_SPEED;
 
-/* CAN Frame Structure */
-typedef struct {
-    uint32_t id;            // CAN ID (11-bit or 29-bit)
-    uint8_t extended;       // 0 = Standard ID, 1 = Extended ID
-    uint8_t rtr;            // Remote Transmission Request
-    uint8_t dlc;            // Data Length Code (0-8)
-    uint8_t data[8];        // Data bytes
-} CAN_Frame;
+
 
 /* Error codes */
 typedef enum {
@@ -137,21 +131,6 @@ typedef enum {
     MCP2515_NOMSG
 } MCP2515_ERROR;
 
-/* CAN Command Codes (data[0] byte for ID 0x72) */
-typedef enum {
-    CMD_CHECK = 0x01,
-    CMD_START = 0x02,
-    CMD_STOP = 0x03,
-    CMD_RESET = 0x04,
-    CMD_SET_END_VOLTAGE = 0x05,
-    CMD_SET_CURRENT = 0x06,
-    CMD_IS_BATT_PRESENT = 0x07,
-    CMD_READ_CURRENT_VOLTAGE = 0x08,
-    CMD_READ_CURRENT_CURRENT = 0x09,
-    CMD_READ_CURRENT_MAH = 0x0A,
-    CMD_READ_CURRENT_POWER = 0x0B,
-    CMD_UNKNOWN = 0xFF
-} CAN_COMMAND;
 
 /* Function Prototypes */
 MCP2515_ERROR MCP2515_Init(SPI_HandleTypeDef *hspi, CAN_SPEED speed);
