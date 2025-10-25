4f57ef99398e71a3289c002ab1b99939fa4806d3
adammarut <adam.marut@icloud.com>
2025-10-25 21:59:50 +0200

    Init CAN id with default value

--- Core/Src/flash_param_store.c
+++ Core/Src/flash_param_store.c
@@ -90,5 +90,9 @@ void ParamStore_Save_CAN_ID(uint8_t id) {
 
 uint8_t ParamStore_Read_CAN_ID(void) {
     FlashParams params = ReadAllParams();
+    if (params.can_id < 0x71 || params.can_id > 0x77) // Validate range
+        params.can_id = 0x77;
+
+    SaveAllParams(params);
     return params.can_id;
 }
