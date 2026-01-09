34dc13d125612e60999945250e528a5ce17b3aa0
adammarut <adam.marut@icloud.com>
2026-01-09 23:13:57 +0100

    refactor: remove unused WS2812 LED control code and header

--- Core/Inc/ws2812c.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "stm32f4xx_hal.h"
-
-#define WS2812_LED_COUNT 6
-
-void WS2812_Send(uint8_t (*colors)[3]);
-void ws2812_enable_dwt(void);
-void WS2812_FillColor(uint8_t r, uint8_t g, uint8_t b);
-void WS2812_SetOne(uint8_t index, uint8_t r, uint8_t g, uint8_t b);
